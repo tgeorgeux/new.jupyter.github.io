988227abfa16ce83f79f2a566908ca50d6cc4172
Kunal Marwaha <marwahaha@berkeley.edu>
2019-10-01 12:31:55 -0700

    Move css and theme to new exported file

--- src/components/Button/index.ts
+++ src/components/Button/index.ts
@@ -2,5 +2,6 @@ import Button from './Button';
 import ButtonMode from './ButtonMode';
 import ButtonText from './ButtonText';
 import ButtonOutline from './ButtonOutline';
+import { getTheme, ButtonTheme } from './ButtonTheme';
 
-export {Button, ButtonMode, ButtonText, ButtonOutline};
+export {Button, ButtonMode, ButtonText, ButtonOutline, ButtonTheme, getTheme};
