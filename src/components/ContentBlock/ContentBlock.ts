86a24eb5f1b2bc3d43d4d895171e6aa53995d885
Kunal Marwaha <marwahaha@berkeley.edu>
2019-10-11 18:34:16 -0700

    Add content block and auto-updating footer

---
+++ src/components/ContentBlock/ContentBlock.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const ContentBlock = styled.div`
+  margin-top: 120px;
+  margin-bottom: 120px;
+  width: 920px;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+export default ContentBlock;
--- src/components/LineBreak/LineBreak.ts
+++ src/components/LineBreak/LineBreak.ts
@@ -1,12 +1,13 @@
 import styled from 'styled-components';
 
 const LineBreak = styled.hr`
-  color: #9e9e9e;
   width: 685px;
-  border-style: solid;
   border-bottom: 0;
   border-left: 0;
-  border-rigth: 0;
+  border-right: 0;
+  border-top: 1px solid #9e9e9e;
+  margin-left: auto;
+  margin-right: auto;
 `;
 
 export default LineBreak;
